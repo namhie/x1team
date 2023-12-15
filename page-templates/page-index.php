354f117c7423a02c2ecd6270e52fb02d8fc5cd5e
Galina <slovotok@yandex.ru>
2023-12-15 17:16:16 +0300

    удаление отступа у .card

--- page-templates/page-index.php
+++ page-templates/page-index.php
@@ -560,7 +560,7 @@ get_header(); // Вставляем заголовок сайта
               </div>
             </div>
             <div class="col-xxl-5 col-lg-6">
-              <div class="card card-body shadow bg-body-tertiary rounded p-3 h-100">
+              <div class="card card-body shadow bg-body-tertiary rounded p-3 pb-0 h-100">
                 <div class="d-flex justify-content-end align-items-end mb-0 mt-0">
                 <div class="toast-container position-fixed top-50 end-0 translate-middle-y p-3">
                   <div class="toast position-sticky top-2" id="liveToast" role="alert" aria-live="assertive" aria-atomic="true" data-bs-autohide="false">
