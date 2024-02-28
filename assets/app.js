41fef9339f5723c216bad507a7b468d1ae15fedd
Galina <slovotok@yandex.ru>
2024-02-28 11:31:40 +0300

    правки кода

--- assets/app.js
+++ assets/app.js
@@ -11,7 +11,7 @@ const sliderImageHorizont = document.querySelector('.slider__image-horizont')
 const youtubes = document.querySelectorAll('.swiper-slide.youtube')
 const ImageYoutubes = document.querySelectorAll('.swiper-slide .youtube')
 const modalItem = document.querySelectorAll('.body .modal')
-const sliderImagesModals = document.querySelectorAll('.slider__images--modal') 
+const sliderImagesModals = document.querySelectorAll('.body .slider__images--modal') 
 
 const mediaQuery = window.matchMedia('(min-width: 769px)')
 
